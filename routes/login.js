0f437c0d2195810810de5b8bd5f91f6d20792cb5
RussellAbraham <russell_a@live.com>
2023-11-18 12:22:59 -0800

    Setup login logic and install cookie-session

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const db = require('../db/connection');
+const { getUserByUserName } = require('../db/queries/users');
 const router  = express.Router();
 
 router.get('/', (req, res) => {
   // template variable for the document title
   res.locals.title = 'Login';
   res.render('login');
+});
+
+router.post('/', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const testIfUserExists = await getUserByUserName(username);
+    if(!testIfUserExists){
+      console.log('username does not exist');
+      return res.status(401).send('Username does not exist');
+    }
+    const passwordMatch = await bcrypt.compare(password, testIfUserExists.password);
+    if (passwordMatch) {
+      console.log('Login success');
+      res.redirect('/');
+    } else {
+      console.error('No match');
+      res.status(401).send('No match');
+    }
+  } catch(err){
+    console.error('Error')
+    res.status(500).send('Error');
+  }
+
 });
 
 module.exports = router;
