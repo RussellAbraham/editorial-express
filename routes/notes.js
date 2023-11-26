7e9e506565dace818672874033094627482b97fd
Jubril Bello <jbello@uoguelph.ca>
2023-11-26 11:31:44 -0500

    refactor notebooks endpoint to separate file

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,6 @@ const bcrypt = require('bcrypt');
 const db = require('../db/connection');
 const { getUserById } = require('../db/queries/users');
 const { getNotes, getNoteById, deleteNoteByNoteId, createNote, updateNote } = require('../db/queries/notes');
-const { getNotebooks, createNewNotebook } = require('../db/queries/notebooks');
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -112,30 +111,5 @@ router.post('/delete/:id', async (req, res) => {
     }
   }
 });
-
-// ---------- Functions for Notebooks -----------------
-
-// Route for handling the form submission to create a new notebook
-router.post('/newNotebook', async (req, res) => {
-  const { title } = req.body;
-  const userId = req.cookies['user_id'];
-
-  // If the user is not logged in, redirect to the login page
-  if (!userId) {
-    return res.redirect('/login');
-  }
-
-  try {
-    // Call a function to create a new notebook in the database
-    await createNewNotebook(userId, title);
-
-    // Redirect the user to the notes page or any other page you prefer
-    res.redirect('/notes');
-  } catch (error) {
-    console.error('Error creating notebook:', error);
-    res.status(500).send('Error creating notebook');
-  }
-});
-
 
 module.exports = router;
