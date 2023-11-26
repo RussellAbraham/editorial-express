a4173b797f525479838b7376cf7acc41f14d19cf
Jubril Bello <jbello@uoguelph.ca>
2023-11-26 10:27:25 -0500

    re-organization of functions for notes/notebooks

--- routes/notes.js
+++ routes/notes.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const db = require('../db/connection');
 const { getUserById } = require('../db/queries/users');
 const { getNotes, getNoteById, deleteNoteByNoteId, createNote, updateNote } = require('../db/queries/notes');
+const { getNotebooks, createNewNotebook } = require('../db/queries/notebooks');
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -23,6 +24,70 @@ router.get("/", async (req, res) => {
   } catch (error) {
     console.error('Error retrieving notes:', error);
     res.status(500).send('Error retrieving notes');
+  }
+});
+
+// Route for reading a specific note
+router.get('/read/:id', async (req, res) => {
+
+  const userId = req.cookies['user_id']
+
+  //if user is not logged in, redirect to the login page
+  if (!userId) {
+    // Redirect to the login page or handle unauthenticated access as appropriate
+    return res.redirect('/login');
+  }
+
+  const noteId = req.params.id;
+
+  const note = await getNoteById(noteId);
+  res.locals.title = 'Editor';
+
+  // Render the editor view with the note content
+  res.render('editor', { note });
+});
+
+// Route for handling the form submission
+router.post('/new', async (req, res) => {
+  const { title } = req.body;
+  const userId = req.cookies['user_id'];
+
+  //if user is not logged in, redirect to the login page
+  if (!userId) {
+    // Redirect to the login page or handle unauthenticated access as appropriate
+    return res.redirect('/login');
+  }
+
+  try {
+    // Call a function to create a new note in the database
+    await createNote(userId, title, '');
+
+    // Redirect the user to the notes page or any other page you prefer
+    res.redirect('/notes');
+  } catch (error) {
+    console.error('Error creating note:', error);
+    res.status(500).send('Error creating note');
+  }
+});
+
+// POST route to handle the update
+router.post('/updateNote', async (req, res) => {
+  console.log("Updating notes");
+
+  try {
+    const { noteId, updatedBody } = req.body;
+    console.log("noteId:", noteId);
+    console.log("updatedBody:", updatedBody);
+    // console.log(req.body)
+
+    // Call your function to update the note in the database
+    await updateNote(noteId, updatedBody);
+
+    // Respond with a success message or any other data as needed
+    res.json({ success: true, message: 'Note updated successfully' });
+  } catch (error) {
+    console.error('Error updating note:', error);
+    res.status(500).json({ success: false, message: 'Error updating note' });
   }
 });
 
@@ -48,114 +113,29 @@ router.post('/delete/:id', async (req, res) => {
   }
 });
 
-// Route for rendering the form
+// ---------- Functions for Notebooks -----------------
 
-// superceded with modal
-// router.get('/new', (req, res) => {
-
-//   res.locals.title = 'New Note';
-//   res.render('newNoteForm');
-// });
-
-// Route for handling the form submission
-router.post('/new', async (req, res) => {
+// Route for handling the form submission to create a new notebook
+router.post('/newNotebook', async (req, res) => {
   const { title } = req.body;
   const userId = req.cookies['user_id'];
 
-  //if user is not logged in, redirect to the login page
+  // If the user is not logged in, redirect to the login page
   if (!userId) {
-    // Redirect to the login page or handle unauthenticated access as appropriate
     return res.redirect('/login');
   }
 
   try {
-    // Call a function to create a new note in the database
-    await createNote(userId, title, '');
+    // Call a function to create a new notebook in the database
+    await createNewNotebook(userId, title);
 
     // Redirect the user to the notes page or any other page you prefer
     res.redirect('/notes');
   } catch (error) {
-    console.error('Error creating note:', error);
-    res.status(500).send('Error creating note');
+    console.error('Error creating notebook:', error);
+    res.status(500).send('Error creating notebook');
   }
 });
-
-// Route for reading a specific note
-router.get('/read/:id', async (req, res) => {
-
-  const userId = req.cookies['user_id']
-
-  //if user is not logged in, redirect to the login page
-  if (!userId) {
-    // Redirect to the login page or handle unauthenticated access as appropriate
-    return res.redirect('/login');
-  }
-
-  const noteId = req.params.id;
-
-  const note = await getNoteById(noteId);
-  res.locals.title = 'Editor';
-
-  // Render the editor view with the note content
-  res.render('editor', { note });
-});
-
-// POST route to handle the update
-router.post('/updateNote', async (req, res) => {
-  console.log("Updating notes");
-
-  try {
-    const { noteId, updatedBody } = req.body;
-    console.log("noteId:", noteId);
-    console.log("updatedBody:", updatedBody);
-    // console.log(req.body)
-
-    // Call your function to update the note in the database
-    await updateNote(noteId, updatedBody);
-
-    // Respond with a success message or any other data as needed
-    res.json({ success: true, message: 'Note updated successfully' });
-  } catch (error) {
-    console.error('Error updating note:', error);
-    res.status(500).json({ success: false, message: 'Error updating note' });
-  }
-});
-
-
-// router.post('/updates-note', async (req, res) => {
-//   try {
-//     // Your updateNote logic here...
-//     console.log(req)
-
-//     res.json({ success: true, message: 'Note updated successfully' });
-//   } catch (error) {
-//     console.error('Error updating note:', error);
-//     res.status(500).json({ success: false, message: 'Error updating note' });
-//   }
-// });
-
-
-
-
-
-
-
-// not used
-// router.get('/:id', async (req, res) => {
-//   res.locals.title = note.title
-
-//   if (!userId) {
-//     // Redirect to the login page or handle unauthenticated access as appropriate
-//     return res.redirect('/login');
-//   } else {
-//     const note = await getNoteById(req.params.id)
-//     const templateVars = {
-//       note
-//     }
-//     res.render('editor', templateVars)
-//   }
-// });
-
 
 
 module.exports = router;
