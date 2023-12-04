bcf065c97e750983e96eb7bab9bcde2a945abb4b
Jubril Bello <jbello@uoguelph.ca>
2023-12-03 19:50:51 -0500

    add POST route for delete

--- routes/notebooks.js
+++ routes/notebooks.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const db = require('../db/connection');
 const { getNotesByNotebookId, getNotesWithoutNotebookByUserId } = require('../db/queries/notes');
-const { getNotebooksByUserId, createNewNotebook } = require('../db/queries/notebooks');
+const { getNotebooksByUserId, createNewNotebook, deleteNotebookById } = require('../db/queries/notebooks');
 
 router.get('/', async (req, res) => {
   const userId = req.cookies['user_id'];
@@ -29,6 +29,21 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Example route for notebook details
+router.get('/catalogue/notebook/:id', async (req, res) => {
+  const notebookId = req.params.id;
+  // Fetch notebook details and associated notes
+  // Render a template with notebook details and associated notes
+});
+
+// Example route for note details
+router.get('/catalogue/note/:id', async (req, res) => {
+  const noteId = req.params.id;
+  // Fetch note details
+  // Render a template with note details
+});
+
+
 // Route for handling the form submission to create a new notebook
 router.post('/newNotebook', async (req, res) => {
   const { title } = req.body;
@@ -46,18 +61,23 @@ router.post('/newNotebook', async (req, res) => {
   }
 });
 
-// Example route for notebook details
-router.get('/catalogue/notebook/:id', async (req, res) => {
+// delete notebooks and notes
+// New route to handle note deletion using POST
+router.post('/delete/:id', async (req, res) => {
   const notebookId = req.params.id;
-  // Fetch notebook details and associated notes
-  // Render a template with notebook details and associated notes
-});
+  const userId = req.cookies["user_id"];
 
-// Example route for note details
-router.get('/catalogue/note/:id', async (req, res) => {
-  const noteId = req.params.id;
-  // Fetch note details
-  // Render a template with note details
+  if (!userId) {
+    res.status(500).send('User currently not logged in!')
+  } else {
+    try {
+      await deleteNotebookById(notebookId);
+      res.redirect('/notebooks'); // Redirect back to the notes page after deletion
+    } catch (error) {
+      console.error('Error deleting note:', error);
+      res.status(500).send('Error deleting note');
+    }
+  }
 });
 
 
